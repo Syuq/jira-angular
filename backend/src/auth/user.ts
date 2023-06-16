3741f1fc7ad14711f1daccca9c9feaf14ab4112c
Syuq <tatd.wine@gmail.com>
2023-06-16 23:24:58 +0700

    ✨ feat(auth): add AuthController and CurrentUser The AuthController is added to handle authentication requests. It has a single POST endpoint that returns the CurrentUser object. The CurrentUser object is a mock user object that contains the user's id, name, avatarUrl, createdAt, and updatedAt.

---
+++ backend/src/auth/user.ts
@@ -0,0 +1,8 @@
+export const CurrentUser = {
+  id: 'd65047e5-f4cf-4caa-9a38-6073dcbab7d1',
+  name: 'Syuq',
+  avatarUrl:
+    'https://res.cloudinary.com/drpatrit3/image/upload/c_scale,w_48/v1685428294/tat_xn9en8.png',
+  createdAt: '2023-06-16T16:00:00.000Z',
+  updatedAt: '2023-06-16T16:00:00.000Z',
+};
