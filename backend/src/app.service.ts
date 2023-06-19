2c4a3a02dde7f1af13cda6071e5d4ed5f81b97a1
Syuq <tatd.wine@gmail.com>
2023-06-19 23:20:51 +0700

    🚚 chore(package.json): rename project name to angular-clone-backend 📝 docs(app.service.ts): update return message to reflect new project name The project name has been changed to angular-clone-backend to better reflect the project's purpose. The return message in the AppService has been updated to reflect the new project name.

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'This is a simple API for Angular Jira Clone app. For more info, visit https://github.com/syuq/jira-angular-clone';
+    return 'This is a simple API for Quan ly du an app. For more info, visit https://github.com/syuq/jira-angular-clone';
   }
 }
