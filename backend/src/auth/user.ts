b038ef3b70bfe8371f27d52277b04324fe3bf033
Syuq <tatd.wine@gmail.com>
2023-06-20 20:29:57 +0700

    🎨 style(user.ts): update user avatar URL The user avatar URL has been updated to a new image. This change is purely cosmetic and does not affect the functionality of the application.

--- backend/src/auth/user.ts
+++ backend/src/auth/user.ts
@@ -2,7 +2,7 @@ export const CurrentUser = {
   id: 'd65047e5-f4cf-4caa-9a38-6073dcbab7d1',
   name: 'Syuq',
   avatarUrl:
-    'https://res.cloudinary.com/drpatrit3/image/upload/c_scale,w_48/v1685428294/tat_xn9en8.png',
+    'https://res.cloudinary.com/drpatrit3/image/upload/v1687252702/36385508eb9e18831af7a4aa25b1a94a_rg1vda.jpg',
   createdAt: '2023-06-16T16:00:00.000Z',
   updatedAt: '2023-06-16T16:00:00.000Z',
 };
