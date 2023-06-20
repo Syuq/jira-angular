7e58ebe601374320ff0fba51efd82a5199224b3c
Syuq <tatd.wine@gmail.com>
2023-06-20 14:41:05 +0700

    🔥 chore(sidebar.ts): remove unused sidebar links The sidebar links for Releases, Issues and filters, Pages, Reports, and Components have been commented out as they are not currently being used. This improves the codebase by removing unused code and simplifying the sidebar.

--- frontend/src/app/project/config/sidebar.ts
+++ frontend/src/app/project/config/sidebar.ts
@@ -3,9 +3,9 @@ import { SideBarLink } from '@syuq/interface/ui-model/nav-link';
 export const SideBarLinks = [
   new SideBarLink('Kanban Board', 'board', 'board'),
   new SideBarLink('Project Settings', 'cog', 'settings'),
-  new SideBarLink('Releases', 'ship'),
-  new SideBarLink('Issues and filters', 'filters'),
-  new SideBarLink('Pages', 'page'),
-  new SideBarLink('Reports', 'report'),
-  new SideBarLink('Components', 'component')
+  // new SideBarLink('Releases', 'ship'),
+  // new SideBarLink('Issues and filters', 'filters'),
+  // new SideBarLink('Pages', 'page'),
+  // new SideBarLink('Reports', 'report'),
+  // new SideBarLink('Components', 'component')
 ];
